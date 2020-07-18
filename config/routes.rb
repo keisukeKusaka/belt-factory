2bb904f9f53c091bb20005727cc97da96525c582
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-19 07:59:45 +0900

    Add search action in products_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "products#index"
-  resources :products, only: [:new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products, only: [:new, :create] do
+    collection do
+      get "search"
+    end
+  end
 end
