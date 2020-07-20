e535cc51c007785daad06ff337281905a197b948
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-21 07:00:18 +0900

    revise "user_department_and_datum?" method
    製品を選択中（@presentが定義されている）のみ記入できる様に条件を追加した。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def user_department_and_datum?(num, datum)
-    current_user.department_id == num && datum.blank?
+    current_user.department_id == num && datum.blank? && @product.present?
   end
 end
