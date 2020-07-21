78dee4211a3f9cbcbda9989fbd28ab3a7e3ea6ec
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-21 11:18:01 +0900

    revise inspection_data_controller.rb
    記述の誤りを修正した。

--- app/controllers/inspection_data_controller.rb
+++ app/controllers/inspection_data_controller.rb
@@ -1,12 +1,12 @@
 class InspectionDataController < ApplicationController
   def create
-    @inspection_datum_new = InspectionDatum.create(production_data_params)
+    @inspection_datum_new = InspectionDatum.create(inspection_data_params)
     redirect_to "/products/#{params[:product_id]}"
   end
 
   private
 
-  def production_data_params
+  def inspection_data_params
     params.require(:inspection_datum).permit(:length, :width, :weight, :comment, :image, :created_at).merge(user_id: current_user.id, product_id: params[:product_id])
   end
 end
