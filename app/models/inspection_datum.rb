279a9e451513a7948ed845b21e7ad45eb30e2ec3
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-27 10:30:42 +0900

    Add uniqueness validation in inspection_datum.product_id
    product_idに一意性制約のバリデーションを追加した。
    inspetion_datum_spec.rbの#8を参照。

--- app/models/inspection_datum.rb
+++ app/models/inspection_datum.rb
@@ -3,6 +3,6 @@ class InspectionDatum < ApplicationRecord
   belongs_to :product
 
   validates :length, :width, :weight, :comment, presence: true
-
+  validates :product_id, uniqueness: true
   mount_uploader :image, ImageUploader
 end
