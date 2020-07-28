1ac3d19cca74d9fcef0d828fd1f22bab47bd06d3
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-29 00:10:30 +0900

    Update products_controller_spec.rb, new action test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -18,4 +18,30 @@ describe ProductsController do
       expect(response).to render_template :index
     end
   end
+
+  describe "#new" do
+    before do
+      get :new
+    end
+
+    it 'new.html.hamlに遷移する' do
+      expect(response).to render_template :new
+    end
+
+    it '@productにProduct.newが代入されている' do
+      expect(assigns(:product)).to be_a_new(Product)
+    end
+
+    it '@clientにClient.allが代入されている' do
+      expect(assigns(:client)).to eq Client.all
+    end
+
+    it '@materialにMaterial.allが代入されている' do
+      expect(assigns(:material)).to eq Material.all
+    end
+
+    it '@new_idに期待される文字列が代入されている' do
+      expect(assigns(:new_number)).to eq ("%06d" % (Product.maximum(:number).to_i + 1))
+    end
+  end
 end
