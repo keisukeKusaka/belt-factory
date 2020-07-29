1da9cfa4c12567fb10d6df4212e665b4606c18fa
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-29 22:51:30 +0900

    Update products_controller_spec.rb, create action test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe ProductsController do
   let(:client) { create(:client) }
+  let(:material) { create(:material) }
   let(:department) { create(:department, id: 2)}
   let(:user) { create(:user, department_id: department.id) }
+  let(:product) { create(:product, client_id: client.id, material_id: material.id, user_id: user.id) }
 
   before do
     login user
@@ -43,5 +45,24 @@ describe ProductsController do
     it '@new_idに期待される文字列が代入されている' do
       expect(assigns(:new_number)).to eq ("%06d" % (Product.maximum(:number).to_i + 1))
     end
+  end
+
+  describe "#create" do
+    before do
+      product_params = attributes_for(:product)
+      post :create, params: {product: product_params}
+    end
+
+    it '@productにProduct.newが代入されている' do
+      expect(assigns(:product)).to be_a_new(Product)
+    end
+
+    it 'new.html.hamlに遷移する' do
+      expect(response).to redirect_to(new_product_path)
+    end
+  end
+
+  describe "search" do
+
   end
 end
