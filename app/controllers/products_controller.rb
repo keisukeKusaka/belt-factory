89df22c0644fa3ab7c684341087cbc14eb3ee7a2
boborabi <65844843+boborabi@users.noreply.github.com>
2020-10-23 02:48:06 +0900

    Update products_controller.rb and routes.rb, add actions (edit, update, destroy)
    edit, update, destroyアクションとルーティングを追加した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,9 @@
 class ProductsController < ApplicationController
   include ApplicationHelper
   load_and_authorize_resource
-  before_action :new_action_only_sales, {only: :new}
+  before_action :new_action_only_sales, only: [:new, :edit]
+  before_action :get_parameters, only: [:new, :edit]
+  before_action :find_product, only: [:show, :edit]
 
   def index
     @products = Product.includes(:production_datum, :inspection_datum, :evaluation_datum, :material, :client).order("created_at DESC").page(params[:page]).per(10)
@@ -9,10 +11,6 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-
-    @client = Client.all
-    @material = Material.all
-
     @new_number = "%06d" % (Product.maximum(:number).to_i + 1)
   end
 
@@ -31,12 +29,23 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
-    product_datum(@product)
     @production_datum_new = ProductionDatum.new
     @inspection_datum_new = InspectionDatum.new
     @evaluation_datum_new = EvaluationDatum.new
-    @products = Product.where(client_id: @product.client_id).order("created_at DESC").page(params[:page]).per(5)
+  end
+
+  def edit
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.update(product_params)
+    redirect_to product_path(product)
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
   end
 
   private
@@ -47,5 +56,16 @@ class ProductsController < ApplicationController
 
   def new_action_only_sales
       redirect_to root_path if current_user.department_id != 2
+  end
+
+  def get_parameters
+    @client = Client.all
+    @material = Material.all
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+    product_datum(@product)
+    @products = Product.where(client_id: @product.client_id).order("created_at DESC").page(params[:page]).per(5)
   end
 end
