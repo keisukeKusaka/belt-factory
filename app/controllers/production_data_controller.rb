2596f8657c200812325f50690054b28da768dd78
boborabi <65844843+boborabi@users.noreply.github.com>
2020-10-26 03:02:06 +0900

    Update production_data_controller.rb, change instance variables names
    インスタンス変数の名称を変更した。

--- app/controllers/production_data_controller.rb
+++ app/controllers/production_data_controller.rb
@@ -2,13 +2,13 @@ class ProductionDataController < ApplicationController
   load_and_authorize_resource
 
   def create
-    @production_datum_new = ProductionDatum.create(production_datum_params)
+    @production_datum = ProductionDatum.create(production_datum_params)
     redirect_to "/products/#{params[:product_id]}"
   end
 
   def update
-    production_datum = ProductionDatum.find(params[:id])
-    production_datum.update(production_datum_params)
+    @production_datum = ProductionDatum.find(params[:id])
+    @production_datum.update(production_datum_params)
     redirect_to product_path(params[:product_id])
   end
 
