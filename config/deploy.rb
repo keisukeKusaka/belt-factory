b9e9758b21dca7ac1693ad58adae68a6c6a2af16
boborabi <65844843+boborabi@users.noreply.github.com>
2020-11-14 05:01:12 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,15 +9,16 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, "2.6.6"
 
-set :ssh_options, auth_methods: ['publickey']
-                  keys: "~/bobokey.pem"
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/bobokey.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-after 'deploy:publishing', 'deploy:restart' namespace :deploy do
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
