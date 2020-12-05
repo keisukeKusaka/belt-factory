c0e16c67100ffdbb17f9ff8bc746a648d16228b8
boborabi <65844843+boborabi@users.noreply.github.com>
2020-12-05 21:10:53 +0900

    Update strage
    テスト時の画像アップロード先をawsから外すように変更した。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,23 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'www.belt-factory.com'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/www.belt-factory.com'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      region: 'ap-northeast-1',
-      path_style: true
-    }
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+else
+  CarrierWave.configure do |config|
+      config.storage :fog
+      config.fog_provider = 'fog/aws'
+      config.fog_directory  = 'www.belt-factory.com'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/www.belt-factory.com'
+      config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+        aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+        region: 'ap-northeast-1',
+        path_style: true
+      }
+  end
 end
