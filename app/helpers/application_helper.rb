8de451f7eaad717a03532c9cff5295eafcc4e6ae
boborabi <65844843+boborabi@users.noreply.github.com>
2020-12-19 23:04:14 +0900

    Update application_helper.rb
    社名と材質名を表示する変数を定義した。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,6 +26,18 @@ module ApplicationHelper
   end
 
   def material_client_present?(params)
+    if params[:client_id].present?
+      @client_name = Client.find(params[:client_id])[:campany]
+    else
+      @client_name = "指定なし"
+    end
+
+    if params[:material_id].present?
+      @material_name = Material.find(params[:material_id])[:name]
+    else
+      @material_name = "指定なし"
+    end
+
     if params[:material_id].present? && params[:client_id].present?
       @products = Product.where(material_id: params[:material_id]).where(client_id: params[:client_id]).order("id DESC").page(params[:page]).per(10)
     elsif params[:material_id].blank? && params[:client_id].present?
